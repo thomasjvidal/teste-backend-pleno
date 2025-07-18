59ad70c65a0ecb5efec275d9698adfabb3663662
thomasjvidal <contatothomasvidal@gmail.com>
2025-07-18 19:30:02 -0300

    chore: remove unused AuthService file

--- routes/api.php
+++ routes/api.php
@@ -24,27 +24,6 @@ Route::get('/test', function() {
     return response()->json(['message' => 'API funcionando!']);
 });
 
-// Rota de debug para contatos (temporária)
-Route::get('/debug/contacts', function() {
-    try {
-        $totalContacts = \App\Models\Contact::count();
-        $contacts = \App\Models\Contact::with(['address', 'phones', 'emails'])->get();
-        
-        return response()->json([
-            'debug_info' => [
-                'total_contacts' => $totalContacts,
-                'database_connection' => 'OK',
-                'contacts_data' => $contacts->toArray()
-            ]
-        ]);
-    } catch (\Exception $e) {
-        return response()->json([
-            'error' => $e->getMessage(),
-            'trace' => $e->getTraceAsString()
-        ], 500);
-    }
-});
-
 // Rotas protegidas
 Route::middleware(['auth:api'])->group(function () {
     // Rotas de autenticação
@@ -53,6 +32,7 @@ Route::middleware(['auth:api'])->group(function () {
     
     // Rotas de contatos (todos os usuários autenticados podem acessar)
     Route::get('/contacts', [ContactController::class, 'index']);
+    Route::get('/contacts/{id}', [ContactController::class, 'show']);
     Route::post('/contacts', [ContactController::class, 'store']);
     
     // Rotas administrativas (apenas ADMIN)
@@ -60,14 +40,4 @@ Route::middleware(['auth:api'])->group(function () {
         Route::put('/contacts/{id}', [ContactController::class, 'update']);
         Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
     });
-    
-    // Rotas específicas para ADMIN (se necessário no futuro)
-    Route::middleware(['checkRole:ADMIN'])->group(function () {
-        // Rotas administrativas específicas podem ser adicionadas aqui
-    });
-});
-
-// Rota padrão do Laravel (mantida para compatibilidade)
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
